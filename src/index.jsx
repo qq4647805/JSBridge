39cf11a803c7f6c464ea57d16aee667bb795a1b1
聋小熊 <shichao.zhou@wenba100.com>
2016-08-30 16:59:37 +0800

    test update

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ class JSBridge {
     this._.cbMap={};
     !glo_cbfn || (window[glo_cbfn]=this.nativeCB)
     !glo_dp || (window[glo_dp]=this.dispatch)
-    alert(navigator.userAgent);
+    alert(/xuebajun\/(\d+)/.exec(navigator.userAgent));
   }
   _(){
     // if(!this._.iframe){
