9b96b30771e20062de1d2c751783ec19a990665b
聋小熊 <shichao.zhou@wenba100.com>
2016-08-30 16:50:33 +0800

    test update

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ class JSBridge {
     this._.cbMap={};
     !glo_cbfn || (window[glo_cbfn]=this.nativeCB)
     !glo_dp || (window[glo_dp]=this.dispatch)
+    console.log(navigator.userAgent);
   }
   _(){
     // if(!this._.iframe){
